039a9b1676d398828939af08596b73e636800616
Michael <65680649+michaelg100@users.noreply.github.com>
2021-08-30 17:52:16 -0400

    Update startdb.sh

--- database/startdb.sh
+++ database/startdb.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run -e "ACCEPT_EULA=Y" -e "SA_PASSWORD=yourStrong(!)Password" -p 1433:1433 -d mcr.microsoft.com/mssql/server:2019-latest
+docker run -e "ACCEPT_EULA=Y" -e "SA_PASSWORD=P(4)ssword" -p 1433:1433 --name sqlserver1 -d mcr.microsoft.com/mssql/server:2019-latest
