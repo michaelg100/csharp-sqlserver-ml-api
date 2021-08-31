fb5092e7fcabdef70b74bfb1e214bf5801224665
Michael <65680649+michaelg100@users.noreply.github.com>
2021-08-30 20:11:46 -0400

    Create start.sql

---
+++ database/start.sql
@@ -0,0 +1,10 @@
+CREATE TABLE main (
+    ID int,
+    Title varchar(255),
+    Book varchar(255) 
+);
+
+INSERT INTO main (ID, Title, Book )
+VALUES(1, 'Sorcer Stone', 'Harry Potter');
+
+SELECT * FROM main;
